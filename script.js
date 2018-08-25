24968fc3a91b8d9d5b9f3e4f83d5049182a60e2f
migueldeguzman <40692613+migueldeguzman@users.noreply.github.com>
2018-08-25 18:46:05 +0800

    initial commit

---
+++ script.js
@@ -0,0 +1,11 @@
+name = prompt("what is your name?");
+
+if (name === "Billy") {
+	alert("Hi Billy!!!");
+} else if 
+	(name === "Susy") {
+	alert("Hi Susy!!!!");	
+} else {
+	alert("I don\'t know you!");
+}
+
